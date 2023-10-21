71661b013bedbb9ef544c838a4eca6932d5d81ad
moyo <moyosxtouch.com>
2023-10-20 21:15:28 -0600

    fin del proyecto

---
+++ src/components/Cotizacion.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  cotizacion: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="contenedor-resultado">
+    <h2>Cotizacion</h2>
+    <div class="resultado">
+      <img
+        :src="'https://cryptocompare.com' + cotizacion.IMAGEURL"
+        alt="imagen cripto"
+      />
+      <div>
+        <p>
+          El precio es de: <span>{{ cotizacion.PRICE }}</span>
+        </p>
+
+        <p>
+          El precio más alto del día: <span>{{ cotizacion.HIGHDAY }}</span>
+        </p>
+        <p>
+          El precio más bajo del día: <span>{{ cotizacion.LOWDAY }}</span>
+        </p>
+        <p>
+          Variación últimas 24 horas:
+          <span>{{ cotizacion.CHANGEPCT24HOUR }}</span>
+        </p>
+        <p>
+          Ultima Actualización: <span>{{ cotizacion.LASTUPDATE }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
